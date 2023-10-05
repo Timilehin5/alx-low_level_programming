e0343cce36716849a0bd48477b70d049b7d3311c
Timilehin5 <olaleyeoluwatimi007@gmail.com>
2023-10-05 02:50:40 -0700

    two

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,9 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-		
+	{
 		i = ci = 0;
+	}
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
@@ -35,7 +36,8 @@ char *str_concat(char *s1, char *s2)
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
-		i++; ci++;
+		i++;
+		ci++;
 	}
 	conct[i] = '\0';
 		return (conct);
